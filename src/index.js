71bb9c66e18d4787c627a109bc60da0cc06360a8
pradeepdevoper <pradeephsaibaba@hotmail.com>
2023-12-28 20:55:30 +0530

    code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-//Write your code here
-
-
-
-
-
-//Do not remove this code
-module.exports = { API }
+function addition(){
+          console.log(arguments[0]);
+          console.log(arguments[1]);
+          //let result = a + b;
+           let result = arguments[0] + arguments[1];
+           console.log(result);
+       }
+       addition(44,56);
+   </script>
+   
+   <script>
+       var length = 50; //var keyword will attach to this
+      // let length = 50; //let will not attach to this keyword
+       function display(){
+           console.log(this.length);
+       }
+       display();
+       
+       module.exports = { API }
